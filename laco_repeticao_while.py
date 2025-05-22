b19630c992ad65aeb78e2adf2f14ef08973bc101
raifernandesz <fernandesdeoliveiradacosta@gmail.com>
2025-05-22 15:44:49 -0300

    Add files via upload

---
+++ laco_repeticao_while.py
@@ -0,0 +1,11 @@
+numero = int(input("Digite seu numero"))
+comeco= int(input("Digite o comeco"))
+fim = int(input("Digite o fim"))
+
+
+
+while comeco <= (fim):
+    print(f"{comeco} x {numero} = {comeco * numero}")
+    comeco += 1
+
+
